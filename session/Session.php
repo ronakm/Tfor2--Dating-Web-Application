849067251e3c363e4abde455c81432f278cb8d4f
ronakm <rmakadiya@gmail.com>
2016-03-27 16:22:55 -0500

    Added files via upload

---
+++ session/Session.php
@@ -0,0 +1,85 @@
+<?php 
+
+ if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
+   /* if(isset($_SESSION['category']))
+	{
+	 $_SESSION['category'];
+	}
+         if(isset($_SESSION['cat_id']))
+	{
+	$_SESSION['cat_id'];
+	}
+       
+        if(isset( $_SESSION['count']))
+	{
+	  $_SESSION['count'];
+	}
+        
+         if(isset( $_SESSION['user']))
+	{
+	  $_SESSION['user'];
+	}
+          if(isset( $_SESSION['cart_entry']))
+	{
+	  $_SESSION['cart_entry'];
+	}
+          if(isset( $_SESSION['cart_products']))
+	{
+	  $_SESSION['cart_products'];
+	}
+         if(isset( $_SESSION['cat']))
+	{
+	  $_SESSION['cat'];
+	}
+         if(isset( $_SESSION['add_cart_error']))
+	{
+	 $_SESSION['add_cart_error'];
+	}
+        
+        if(isset($_SESSION['no_of_row']))
+	{
+	 $_SESSION['no_of_row'];
+	}
+        
+         if(isset($_SESSION['row_id']))
+	{
+	 $_SESSION['row_id'];
+	}
+        if(isset($_SESSION['order_error']))
+	{
+	$_SESSION['order_error'];
+	}
+        
+        if(isset($_SESSION['view_cart_error']))
+	{
+	$_SESSION['view_cart_error'];
+	}
+        if(isset($_SESSION['random']))
+	{
+	 $_SESSION['random'];
+	}
+        if(isset($SESSION['captcha_error']))
+	{
+	$SESSION['captcha_error'];
+	}
+        */
+        if(isset($_SESSION['search']))
+	{
+	  $_SESSION['search'];
+	}
+        
+       /* if(isset($_SESSION['cat_name']))
+	{
+	  $_SESSION['cat_name'];
+	}
+        */
+         
+        
+      
+        
+      
+
+?>
