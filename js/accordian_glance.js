41757d49b8cb45c5e14829e3520d2bfa5b43d339
ronakm <rmakadiya@gmail.com>
2016-03-27 16:19:54 -0500

    Added files via upload

---
+++ js/accordian_glance.js
@@ -0,0 +1,73 @@
+// hover effect
+$(document).ready(function() {
+  $('div.demo-show p').add('div.demo-show2 p').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
+});
+
+// independently show and hide
+$(document).ready(function() {
+  $('div.demo-show:eq(0) > div').hide();  
+  $('div.demo-show:eq(0) > p').click(function() {
+     alert("ddd");
+    $(this).next().slideToggle('fast');
+  });
+});
+
+// one showing at a time
+
+$(document).ready(function() {
+  $('div.demo-show:eq(1) > div:gt(0)').hide();  
+  $('div.demo-show:eq(1) > p').click(function() { 
+      alert("ddd");
+    $(this).next('div:hidden').slideDown('fast')
+    .siblings('div:visible').slideUp('fast');
+  });
+});
+
+
+//simultaneous showing and hiding
+
+$(document).ready(function() {
+  $('div.demo-show2:eq(0) > div').hide();
+  $('div.demo-show2:eq(0) > p').click(function() { 
+     
+      if($(this).children('a').text()=='Edit')
+      { 
+          $('div.demo-show2:eq(0) > p a').html("Edit");
+          $(this).children('a').html('Close');
+      }
+      else
+      {    
+           $(this).children('a').html('Edit');
+      }
+      
+      
+    $(this).next('div').slideToggle('fast')
+    .siblings('div:visible').slideUp('fast');
+  });
+});
+
+//queued showing and hiding
+$(document).ready(function() {
+  $('div.demo-show2:eq(1) > div').hide();  
+  $('div.demo-show2:eq(1) > p').click(function() { 
+       alert("ddd");
+    var $nextDiv = $(this).next();
+    var $visibleSiblings = $nextDiv.siblings('div:visible');
+
+    if ($visibleSiblings.length ) {
+      $visibleSiblings.slideUp('fast', function() {
+       
+        $nextDiv.slideToggle('fast');
+      });
+    } else {
+        alert("sss");
+       $nextDiv.slideToggle('fast');
+    }
+  });
+});
+
+
---
+++ js/at_my_glance.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+        
+     $("#height .features").click(function() 
+    {
+        
+        
+        if ($('#height div').is(':hidden'))
+        { 
+            $("#height .options").show();
+        }
+        else
+        {
+          $("#height .options").hide();
+        }
+       
+    });
+    
+    $("#height .value").click(function() 
+    {
+        
+        
+        if ($('#height div').is(':hidden'))
+        { 
+            $("#height .options").show();
+        }
+        else
+        {
+          $("#height .options").hide();
+        }
+        
+    });
+    
+   
+   
+   
+    
+});
